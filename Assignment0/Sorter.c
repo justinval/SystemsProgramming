0432b9d30aace0c26f6d5eb236542e61323115db
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-09-24 17:19:10 -0400

    Editted first conditional Sorter.c, and added new lines to errors. And testing stdin and stdout functionality.

--- Assignment0/Sorter.c
+++ Assignment0/Sorter.c
@@ -8,20 +8,25 @@ int main (int argc, char *argv[])
 {
 	//Check to see if there is the proper number of params
 	if (argc == 3) 
-	{	
-		char *sortByParam = argv[1];
-
+	{	 
 		//Check to see if 1st param is "-c"
-		if (strcmp(sortByParam, "-c") != 0) {
-			printf("Can only sort by column. Please try again.");
+		if (strcmp(argv[1], "-c") != 0) {
+			printf("Can only sort by column. Please try again. \n");
 			return 0;
 		}
+
+		//Read next line of CSV file into str[100]
+		char str[100];
+		gets(str);
+		printf("%s", str);
+
+		return 0;
     }
 
     //If the user didn't have the proper number of params
     else
     {
-    	printf("Invalid number of inputs. Please try again.");
+    	printf("Invalid number of inputs. Please try again. \n");
     	return 0;
     }
 }
