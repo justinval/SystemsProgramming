b3e3cd60515f727dec833d50aa0b78eceb760f96
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-09-27 00:29:27 -0400

    test

--- Assignment0/Sorter.c
+++ Assignment0/Sorter.c
@@ -24,7 +24,7 @@ int main (int argc, char *argv[])
 		Movie *ptr = (Movie *)malloc(sizeof(Movie));
 		Movie **movieList = (Movie **)malloc(MAX * sizeof(Movie *));
 		int numOfMovies = 0;
-
+		printf("test");
 		//Loops until scanf is unsuccessful/reaches EOF
 		while (scanf("%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,%s,%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i",
 				&ptr->color, &ptr->director_name, &ptr->num_critic_for_reviews, &ptr->duration, &ptr->director_facebook_likes,
