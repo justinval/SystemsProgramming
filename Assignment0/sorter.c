f6d3384edf20f5797b40efec7353a8020d07a9ac
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-09-28 12:09:01 -0400

    no message

--- Assignment0/sorter.c
+++ Assignment0/sorter.c
@@ -135,7 +135,8 @@ char *strtokPlus (char *str, const char *delim)
 	else 
 	{		
 		temp = (strstr(str, delim));
-		index = temp - str; 
+		index = temp - str;
+		token = (char *)malloc(index * sizeof(char)); 
 		memcpy(token, str, index);
 
 		//If str still has commas
