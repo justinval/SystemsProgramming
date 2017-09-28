5b0ad856e072f3a94b80120aa96c970bcd46755d
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-09-28 19:05:17 -0400

    no message

--- Assignment0/sorter.c
+++ Assignment0/sorter.c
@@ -18,7 +18,7 @@ int main (int argc, char *argv[])
 		}
 
 		//Read format line of CSV file into str[100]
-		char str[2000];
+		char *str = (char *)malloc(2000 * sizeof(char));
 		scanf("%s\n", str);				
 		
 		//Initializes ptr and movieList
