47562a6dd8c700094a09f5e12073a9347644d45b
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-10-02 17:50:42 -0400

    no message

--- Assignment0/mergesort2.c
+++ Assignment0/mergesort2.c
@@ -276,25 +276,25 @@ void merge(Movie *arr[], int l, int m, int r, char* field)
             {
         		if(L[i]->movie_title == NULL)
                 {
-                    printf("K = %i | Compared L[%i]:NULL & R[%i]:%s \n | Result is: 1", k, i, j, R[i]->movie_title);
+                    printf("K = %i | Compared L[%i]:NULL & R[%i]:%s | Result is: 1\n", k, i, j, R[i]->movie_title);
         			arr[k] = L[i];
         			i++;
         		}
         		else if(R[j]->movie_title == NULL)
                 {
-                    printf("K = %i | Compared L[%i]:%s & R[%i]:NULL \n | Result is: 2", k, i, L[i]->movie_title, j);
+                    printf("K = %i | Compared L[%i]:%s & R[%i]:NULL | Result is: 2\n", k, i, L[i]->movie_title, j);
         			arr[k] = R[j];
         			j++;
         		}
         		else if (strcmp(L[i]->movie_title, R[j]->movie_title)<1)
                 {
-                    printf("K = %i | Compared L[%i]:%s & R[%i]:%s \n | Result is: 3", k, i, L[i]->movie_title, j, R[i]->movie_title);
+                    printf("K = %i | Compared L[%i]:%s & R[%i]:%s | Result is: 3\n", k, i, L[i]->movie_title, j, R[i]->movie_title);
            			arr[k] = L[i];
             		i++;
         		}
         		else
                 {
-                    printf("K = %i | Compared L[%i]:%s & R[%i]:%s \n | Result is: 4", k, i, L[i]->movie_title, j, R[i]->movie_title);
+                    printf("K = %i | Compared L[%i]:%s & R[%i]:%s | Result is: 4 \n", k, i, L[i]->movie_title, j, R[i]->movie_title);
             		arr[k] = R[j];
         			j++;
         		}
