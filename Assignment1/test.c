0c9e24cfae5e94faaa23d3f2c618e6a7a1a0774c
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-11-01 21:54:40 -0400

    no message

--- Assignment1/test.c
+++ Assignment1/test.c
@@ -8,76 +8,10 @@
 #include "mergesort.c"
 #include <dirent.h>
 
-void traverseDir (char *targetDir) 
-{
-	DIR *dir, *subDir;
-	struct dirent *ent;
-
-	if ((dir = opendir(targetDir)) != NULL) 
-	{
-		while ((ent = readdir(dir)) != NULL) 
-		{
-			// Create a path for each directory entry
-			char *path = (char *)malloc(1024 * sizeof(char));
-			strcpy(path, targetDir);
-			strcat(path, "/");
-			strcat(path, ent->d_name);
-
-			// Try to open each path, and if successful, it's a directory
-			if ((subDir = opendir(path)) != NULL)
-			{	
-				closedir(subDir);
-				if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0 || strcmp(ent->d_name, ".git") == 0){
-                	continue;
-				}
-				printf("found directory: %s \n", path);
-				
-				// Use the child process to traverse the found directory
-				pid_t pid = fork();
-				if (pid == 0)
-				{
-					traverseDir(path);
-					return;
-				}
-				if (pid > 0)
-				{
-					printf("Parent process: %i \n", pid);
-				}
-			}
-
-			// If the directory entry is a csv file
-			else if (strstr(ent->d_name, ".csv") != NULL) 
-			{
-				printf("Found CSV file: %s \n", path);
-
-				//fork ()
-
-
-			}
-			free(path);
-		}
-		closedir (dir);
-	}
-	else 
-	{
-	  /* could not open directory */
-	  	perror ("");
-		return;
-	}
-
-	int status;
-	if (wait(&status) >= 0)
-	{
-        printf("Child process exited with %d status\n", WEXITSTATUS(status));
-	}
-	return;
-
-
-}
-
 int main (int argc, char *argv[]) 
 {
-	char *targetDir = argv[1];
-	traverseDir(targetDir);
-	return 0;
+	printf("%d \n", getpid());
+
+	fork();
+	printf("%d \n", getpid());
 }
