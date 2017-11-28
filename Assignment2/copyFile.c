4dc56567208abd239aa9f99b9cb3172bf231aefb
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-11-28 15:53:12 -0500

    no message

--- Assignment2/copyFile.c
+++ Assignment2/copyFile.c
@@ -3,20 +3,19 @@
 #include <sys/stat.h>
 #define NUMFILES 1
 #define NUMDIR 1 
-//#define FILENAME "movie_metadata.csv"
-//#define DIRNAME "someDir"
+#define FILENAME "movie_metadata.csv"
+#define DIRNAME "someDir"
 
 int main (int argc, char *argv[]) {
-	printf("test \n");
-	//mkdir("someDir", 0777);
-	printf("test \n");
+	mkdir(DIRNAME, 0777);
+	printf("test");
 
-	FILE *file = fopen("movie_metadata.csv", "r");	
+	FILE *file = fopen(FILENAME, "r");	
 	char *tempFileName =  (char *)malloc(10 * sizeof(char));
 	int i;
 	for (i = 0; i < NUMFILES; i++)
 	{
-		sprintf(tempFileName, "%s/%d.txt", "someDir", i);
+		sprintf(tempFileName, "%s/%d.txt", DIRNAME, i);
 		FILE *destFile = fopen(tempFileName, "w");
 		char c;
 		while ((c = fgetc(file)) != EOF)
