b8fefbab205c90865716a9453100709e379aef4d
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-11-29 16:33:34 -0500

    no message

--- Assignment2/copyFile.c
+++ Assignment2/copyFile.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#define NUMFILES 8
+#define NUMFILES 16
 #define NUMDIR 1 
 #define FILENAME "movie_metadata.csv"
-#define DIRNAME "someDir/test4"
+#define DIRNAME "someDir/test5"
 
 int main (int argc, char *argv[]) {
 	mkdir(DIRNAME, 0777);	
