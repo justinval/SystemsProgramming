20a21655a1d4ced3159713b700450ce4fdc2cc18
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-11-29 18:48:21 -0500

    no message

--- Assignment2/sorter.c
+++ Assignment2/sorter.c
@@ -428,6 +428,7 @@ void *sortDir (void *ptrIn)
 				
 				// Create new thread to traverse the found directory
 				t1 = pthread_create(&dtid, NULL, sortDir, (void *)sortDirParams);
+				print("ThreadID: %i", gettid());
 				// Waits for the newly created thread to terminate before continuing
 				t2 = pthread_join(dtid, NULL);
 			}
@@ -453,6 +454,7 @@ void *sortDir (void *ptrIn)
 
 				// Use the child process to sort the found CSV file
 				t3 = pthread_create(&ftid, NULL, sortFile, (void *)sortFileParams);
+				print("ThreadID: %i", gettid());
 				// Waits for the newly created thread to terminate before continuing				
 				t4 = pthread_join(ftid, NULL);
 
