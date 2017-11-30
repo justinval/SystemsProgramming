99436e953632295f9d85300b1e223c75da654f30
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-11-29 19:34:25 -0500

    no message

--- Assignment1/sorter.c
+++ Assignment1/sorter.c
@@ -244,10 +244,10 @@ void printAllCSVFile (Movie **movieList, char *fileDirPath, char *filePath, char
 	}
 	else
 	{
+		printf("OutputDir: %s\n", outputDir);
+		printf("OutputFileName: %s\n", outputFileName);
 		strcat(outputDir, "/");
 		strcat(outputDir, outputFileName);
-		printf("OutputDir: %s", outputDir);
-		printf("OutputFileName: %s", outputFileName);
 		outputFile = fopen(outputDir, "w");
 	}
 	
