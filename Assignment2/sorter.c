5c31bafdb6aa98faa5677127331943d43ca1cabf
Justin Valeroso <jav191@scarletmail.rutgers.edu>
2017-12-19 16:20:09 -0500

    no message

--- Assignment2/sorter.c
+++ Assignment2/sorter.c
@@ -522,7 +522,7 @@ void *sortDir (void *ptrIn)
 				sortFileParams->threadsFile = file;
 
 				// Create a thread to sort the found CSV file
-				printf("%s \n", outputFileName);
+				printf("%s \n", path);
 				t3 = pthread_create(&ftid, NULL, sortFile, (void *)sortFileParams);
 
 				// Waits for the newly created thread to terminate before continuing				
